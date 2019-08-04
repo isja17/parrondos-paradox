1014b529b23ba6c50769d1f6c62e36f222d68935
isaac allison <isja17@hampshire.edu>
2019-08-03 23:23:55 -0400

    Updated README

--- parrondos_paradox.py
+++ parrondos_paradox.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# This is a program that lets you test three different Parrondos Paradox strategies. Parrondos Paradox is a game theory paradox that states that there exists two losing games such that, if you play them in alternating orders, you will end up with a winning record. The two games are as follows. In game a, you have a .5 - epsilon chance to win. In the second game, if the amount of money you have is divisible by three, you have a .1-epsilon chance to win; otherwise, you have a .75-epsilon chance to win. If you were to play only one of these games, you would end up losing.
+# This is a program that lets you test three different Parrondos Paradox strategies. Parrondos Paradox is a game theory paradox that states that there exists two losing games 
+#such that, if you play them in alternating orders, you will end up with a winning record. The two games are as follows. In game a, you have a .5 - epsilon chance to win. 
+#In the second game, if the amount of money you have is divisible by three, you have a .1-epsilon chance to win; otherwise, you have a .75-epsilon chance to win. 
+#If you were to play only one of these games, you would end up losing.
 # 
-# I include two other strategies beyond alternating games. The second strategy picks a random game to play. The third plays game a twice, then game b twice, etc. In all of these strategies, we expect to have a winning rate by the end, provided we play enough games. 
+# I include two other strategies beyond alternating games. The second strategy picks a random game to play. The third plays game a twice, then game b twice, etc. 
+#In all of these strategies, we expect to have a winning rate by the end, provided we play enough games. 
 # 
 # Parrondos Paradox: https://en.wikipedia.org/wiki/Parrondo%27s_paradox#The_coin-tossing_example
 
